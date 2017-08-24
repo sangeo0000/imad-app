408f64ec3fb185f383da2e431a9edf0ed01b9eda
sangeo0000 <31108679+sangeo0000@users.noreply.github.com>
2017-08-25 00:32:12 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -5,7 +5,7 @@ var img = document.getElementById("madi");
 var marginLeft=0;
 function moveRight(){
   marginLeft= marginLeft +10;
-  img.style.marginLeft = marginleft + 'px';
+  img.style.marginLeft = marginLeft + 'px';
 }
 
 img.onclick = function(){
